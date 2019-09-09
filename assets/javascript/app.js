1c7d3d1837bdab8639bfe69d274ac704c1b8a57b
Da Vinci <jeb2je@gmail.com>
2019-09-08 21:42:13 -0400

    Question and time displays on page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
-console.log($);
+var counter = 30;
+var currentQuestion = 1;
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var score = 0; 
+var timer;
+
+//Question and answer choices
+
+function displayQuestion() {
+    
+    var question = triviaQuestions [currentQuestion].question;
+    var options = triviaQuestions [currentQuestion].options;
+
+    //time display
+    $(time).html('Time: ' + counter +" secs");
+
+    $('#game').html('<p>' + question + '</p>');
+}
+
+displayQuestion();
---
+++ assets/javascript/questions.js
@@ -0,0 +1,20 @@
+var triviaQuestions = [
+    {
+        question: "What quarterback owns the all-time passing yardage record?",
+        options: ["Tom Brady", "Peyton Manning", "Drew Brees", "Aaron Rodgers"],
+        answer: "Drew Brees"
+    },
+
+    {
+        question: "The symbol <strong>Pb</strong>, represents what chemical element?",
+        options: ["Plutonium", "Lead", "Phosphorus", "Calcium"],
+        answer: "Lead"
+    },
+
+    {
+        question: "What is the largest planet in the Earth's solar system?",
+        options: ["Saturn", "Uranus", "Neptune", "Jupiter"],
+        answer: "Jupiter"
+    },
+
+];
